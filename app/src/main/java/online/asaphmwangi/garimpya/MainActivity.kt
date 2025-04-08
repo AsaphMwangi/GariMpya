445dff8baaf817b5f1a4edcac88d60cc78f7a869
Asaph <asaphmwangi1973@gmail.com>
2025-04-08 07:24:04 +0300

    Hello new Update

--- app/src/main/java/online/asaphmwangi/garimpya/MainActivity.kt
+++ app/src/main/java/online/asaphmwangi/garimpya/MainActivity.kt
@@ -1,12 +1,17 @@
 package online.asaphmwangi.garimpya
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
